62935a6c54db3fb59631fdaaef59fed5275726c6
alexholamov <61333588+alexholamov@users.noreply.github.com>
2020-04-24 20:51:16 +0300

    Add files via upload

---
+++ users.py
@@ -0,0 +1,86 @@
+import datetime
+# импортируем библиотеку sqlalchemy и некоторые функции из нее 
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# константа, указывающая способ соединения с базой данных
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+# базовый класс моделей таблиц
+Base = declarative_base()
+
+class User(Base):
+    """
+    Описывает структуру таблицы user для хранения регистрационных данных пользователей
+    """
+    # задаем название таблицы
+    __tablename__ = 'user'
+    # идентификатор пользователя, первичный ключ
+    id = sa.Column(sa.Integer, primary_key=True)
+    # имя пользователя
+    first_name = sa.Column(sa.Text)
+    # фамилия пользователя
+    last_name = sa.Column(sa.Text)
+    # пол пользователя
+    gender = sa.Column(sa.Text)
+    # адрес электронной почты пользователя
+    email = sa.Column(sa.Text)
+    # дата рождения пользователя
+    birthdate = sa.Column(sa.Text)
+    # рост пользователя
+    height = sa.Column(sa.Float)
+
+def connect_db():
+    """
+    Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии 
+    """
+    # создаем соединение к базе данных
+    engine = sa.create_engine(DB_PATH)
+    # создаем описанные таблицы
+    Base.metadata.create_all(engine)
+    # создаем фабрику сессию
+    session = sessionmaker(engine)
+    # возвращаем сессию
+    return session()
+
+def request_data():
+    """
+    Запрашивает у пользователя данные и добавляет их в список users
+    """
+    # выводим приветствие
+    print("Здравствуйте! Сейчас мы внесём Ваши данные в базу олимпиады!")
+    # запрашиваем у пользователя данные
+    first_name = input("Введите Ваше имя: ")
+    last_name = input("Введите Вашу фамилию: ")
+    gender = input("Укажите Ваш пол (М/Ж): ")
+    email = input("Ваш адрес электронной почты: ")
+    birthdate = input("Полная дата рождения (ГГГГ-ММ-ДД): ")
+    height = input("Ваш рост (в метрах, при этом значение отделить точкой, например 1.8): ")
+
+    # создаем нового пользователя
+    user = User(
+        first_name=first_name,
+        last_name=last_name,
+        gender=gender,
+        email=email,
+        birthdate=birthdate,
+        height=height
+    )
+    # возвращаем созданного пользователя
+    return user
+
+def main():
+    """
+    Осуществляет взаимодействие с пользователем, обрабатывает пользовательский ввод
+    """
+    session = connect_db()
+    # просим пользователя выбрать режим
+    user = request_data()
+    # добавляем нового пользователя в сессию
+    session.add(user)
+    # сохраняем все изменения, накопленные в сессии
+    session.commit()
+    print("Спасибо, данные сохранены в базе олимпиады!")
+
+if __name__ == "__main__":
+    main()
